4bf3f517deb7a313ab3b78c5c982fe4a34c3e06c
Pozsa Marcell (SZF_E_2022_2024) <pozsa.marcell@diak.szamalk-szalezi.hu>
2023-04-29 09:13:25 +0200

    petinek puska

--- Szemely.js
+++ Szemely.js
@@ -1,6 +1,6 @@
 class Szemely {
   #szulNev;
-  constructor(nev, szulDatum) {
+  constructor(nev, szulDatum, szuloElem) {
     //beállítja az adattagokat
     //a konkrét osztály példányra mutat a this
     //osztál példány ==objektum
@@ -9,7 +9,12 @@ class Szemely {
     this.szulDatum = szulDatum;
     this.#szulNev = nev;
     this.setSzulnev(nev)
-
+    //htmL OLDALON AKARJUK MEGJELENITENI
+    szuloElem.append(`<div class="szemely">
+                        <h3>${this.nev}</h3>
+                        <p>${this.kor()}</p>
+                        </div>
+    `)
     //privát adattagok elérése
   }
   getSzulnev() {
@@ -18,6 +23,11 @@ class Szemely {
   setSzulnev(ertek){
     /**ellenőrzés: nagybetű ,nem szám stb */
     this.#szulNev=ertek
+  }
+  kor(){
+    const d =new Date()
+    let year =d.getFullYear()
+    return year -this.szulDatum
   }
 }
 
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import Szemely from "./Szemely.js";
 
 $(function(){
-    console.log("hellóka nyaloka")
+    const szuloElem=$(".tarolo")
     //példányosítás
-    const szemely1= new Szemely("Béla",1965)
-    const szemely2= new Szemely("Jolán",1975)
+    const szemely1= new Szemely("Béla",1965,szuloElem)
+    const szemely2= new Szemely("Jolán",1975,szuloElem)
 
     console.log(szemely1);
     console.log(szemely2)
@@ -17,6 +17,7 @@ $(function(){
     //futásidőben változtathatóak az osztály példányok
     //console.log(szemely1.#szulNev) kivülről privát adattagot nem érek el
     console.log(szemely1.getSzulnev())
+    console.log(szemely1.kor())
 
 
 })
